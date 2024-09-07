1737a275aff683936fcac0d64e456203e9c888a3
Irtiza Shahbaz <shahbaz4ever17@gmail.com>
2024-09-08 01:46:22 +0500

    Add Files ☀👾

---
+++ script.js
@@ -0,0 +1,97 @@
+// Question 01
+
+// var input = prompt("Enter your input:"); 
+
+// if (input >= '0' && input <= '9') {
+//     console.log("Number");
+// } else if (input >= 'A' && input <= 'Z') {
+//     console.log("Uppercase Letter");
+// } else if (input >= 'a' && input <= 'z') {
+//     console.log("Lowercase Letter");
+// } else {
+//     console.log("Invalid Input");
+// }
+
+// Question 02
+
+// var num1 = prompt("Enter number 1:");
+
+// var num2 = prompt("Enter number 2:");
+
+// if (num1 > num2) {
+//     console.log(num1 + " is larger");
+// } else if (num2 > num1) {
+//     console.log(num2 + " is larger");
+// } else {
+//     console.log("Both numbers are equal");
+// }
+
+// Question 03
+
+// var number = prompt("Enter a number:");
+
+// if (number > 0) {
+//     console.log("The number is positive");
+// } else if (number < 0) {
+//     console.log("The number is negative");
+// } else {
+//     console.log("The number is zero");
+// }
+
+
+// Question 04
+
+// var char = prompt("Enter a character:");
+
+// if (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u') {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+// Question 05
+
+// var correctPassword = "mySecret123";
+// var userPassword = prompt("Enter your password:");
+
+// if (!userPassword) {
+//     console.log("Please enter your password");
+// } else if (userPassword === correctPassword) {
+//     console.log("Correct! The password you entered matches the original password");
+// } else {
+//     console.log("Incorrect password");
+// }
+
+
+
+// Question 06
+
+// var greeting;
+// var hour = 13;
+
+// if (hour < 18) {
+//     greeting = "Good day";
+// } else {
+//     greeting = "Good evening";
+// }
+
+// console.log(greeting); 
+
+
+
+// Question 07
+
+// var time = prompt("Enter time in 24-hour format (e.g. 1900 for 7pm):");
+
+// if (time >= 0 && time < 1200) {
+//     console.log("Good Morning");
+// } else if (time >= 1200 && time < 1700) {
+//     console.log("Good Afternoon");
+// } else if (time >= 1700 && time < 2100) {
+//     console.log("Good Evening");
+// } else if (time >= 2100 && time <= 2359) {
+//     console.log("Good Night");
+// } else {
+//     console.log("Invalid time input");
+// }
